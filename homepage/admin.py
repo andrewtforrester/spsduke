4f7d54177b972c5fa25310b5ee53a00522b6e2ff
Andrew Forrester <atf@atf-mbp.local>
2020-02-02 14:54:19 -0500

    note

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from homepage.models import *
 
 # Register your models here.
+
+admin.site.register(ExecMember)
+admin.site.register(Project)
+admin.site.register(BlogPost)
+admin.site.register(Event)
