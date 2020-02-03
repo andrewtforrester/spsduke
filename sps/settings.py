9166ce49d5394c27793f30f9f5368b200622853c
Andrew Forrester <atf@fastmail.com>
2020-02-03 04:59:53 +0000

    server side updates

--- sps/settings.py
+++ sps/settings.py
@@ -14,7 +14,7 @@ import os
 import json
 
 with open('/etc/config.json') as config_file:
-    config = json.load(config)file)
+    config = json.load(config_file)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -29,7 +29,7 @@ SECRET_KEY = config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.105.157.135']
 
 
 # Application definition
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/atf/spsduke/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
