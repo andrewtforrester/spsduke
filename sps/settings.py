1dcfdf3ccc02cd775fbe0e32d8849d48d78143f0
Andrew Forrester <atf@fastmail.com>
2020-02-12 10:38:16 +0000

    note

--- sps/settings.py
+++ sps/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1','172.105.157.135']
+DEBUG = False
+ALLOWED_HOSTS = ['172.105.157.135','spsduke.com','www.spsduke.com']
 
 
 # Application definition
